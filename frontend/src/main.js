428dcd6a17be97a6a65465d03afed70c173253df
Risto Kuusterä <risto.kuustera@gofore.com>
2017-08-25 01:35:05 +0300

    Bye Bye bus, welcome vuex

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,15 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VeeValidate from 'vee-validate'
+import store from './store/store'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
 
 Vue.use(VeeValidate)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   template: '<App/>',
   components: { App }
