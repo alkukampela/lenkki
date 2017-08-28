083793889055f259e24d190a5c05022cac66456f
Risto Kuusterä <risto.kuustera@gofore.com>
2017-08-28 21:43:41 +0300

    Length of the route moved to Vuex to make room for magic button

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,19 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { SET_START_LOCATION, CLEAR_START_LOCATION } from './mutation-types.js'
+import {
+  SET_START_LOCATION,
+  CLEAR_START_LOCATION,
+  SET_ROUTE_LENGTH
+} from './mutation-types.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    startLocation: undefined
+    startLocation: null,
+    routeLength: null
   },
   mutations: {
     [SET_START_LOCATION](state, location) {
       state.startLocation = location
     },
     [CLEAR_START_LOCATION](state) {
-      state.startLocation = undefined
+      state.startLocation = null
+    },
+    [SET_ROUTE_LENGTH](state, length) {
+      state.routeLength = parseFloat(length)
+    }
+  },
+  getters: {
+    canSend: state => {
+      return !!state.routeLength && !!state.startLocation
     }
   }
 })
